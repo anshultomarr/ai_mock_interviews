3296066bacc1cc6759a51389d2263a3261edc848
anshul <anshultomar63@gmail.com>
2025-03-28 15:34:15 +0530

    fix 1 error at route.ts

--- app/api/vapi/route.ts
+++ app/api/vapi/route.ts
@@ -8,7 +8,7 @@ export async function GET() {
     return Response.json({success: true, data: 'THANK YOU!'}, {status: 200});
 }
 
-export async fuction POST({request}: Request) {
+export async function POST({request}: Request) {
     const { type, role, level, techstack, amount, userid} = await request.json();
 
     try {
