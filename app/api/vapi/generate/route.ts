9cb4e0165a8ea136850257915d1d972688409f04
anshul <anshultomar63@gmail.com>
2025-03-30 16:55:37 +0530

    implementing feedback feature

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -8,7 +8,7 @@ export async function GET() {
     return Response.json({success: true, data: 'THANK YOU!'}, {status: 200});
 }
 
-export async function POST({request}: Request) {
+export async function POST(request: Request) {
     const { type, role, level, techstack, amount, userid} = await request.json();
 
     try {
@@ -41,10 +41,10 @@ export async function POST({request}: Request) {
         
         await db.collection("interviews").add(interview);
 
-        Response.json({success: true}, {status: 200})
+        return Response.json({success: true}, {status: 200});
     } catch (error) {
-        console.log(error);
+        console.error("Error:",error);
 
-        return Response.json({success: false, error}, {status: 500});
+        return Response.json({success: false, error: error}, {status: 500});
     }
 }
